fb72fec4bf3a0ad506b2719a26005dd1d9ffaf1a
zzanguchoisk <chltjsrkd@hanmail.net>
2021-05-16 00:36:46 +0900

    Initial commit

---
+++ src/main/java/com/example/mvcframworkmake/MvcFramworkMakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcframworkmake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcFramworkMakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcFramworkMakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvcframworkmake/MvcFramworkMakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcframworkmake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcFramworkMakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
